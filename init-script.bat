8af4efdbf3e522d2cdd0176019435fe019bbed82
Khanh Trang <jng.151023@gmail.com>
2024-04-14 03:13:00 +0700

    feat: add init script for automating folder structure creating process

---
+++ init-script.bat
@@ -0,0 +1,7 @@
+echo "--- Odoo Environment Init ---"
+echo "Creating needed folders..."
+mkdir .\data\odoo\config
+mkdir .\data\odoo\data
+mkdir .\data\odoo\addons
+mkdir .\data\postgres
+echo "Done. Now you can start docker compose!"
