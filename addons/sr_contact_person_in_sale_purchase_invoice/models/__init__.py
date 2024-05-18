cc181d74dfb7d7b57620f5a86a8c5cb84d65e392
gitName <a@b.c>
2024-05-18 19:28:01 +0700

    Refactor project for development

---
+++ addons/sr_contact_person_in_sale_purchase_invoice/models/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) Sitaram Solutions (<https://sitaramsolutions.in/>).
+#
+#    For Module Support : info@sitaramsolutions.in  or Skype : contact.hiren1188
+#
+##############################################################################
+
+from . import sr_inherited_sale_order
+from . import sr_inherited_invoice_order
+from . import sr_inherited_purchase_order
