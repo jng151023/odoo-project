d586d187de05fea98032afbeede0e8ea2b97cf2d
Tuyet-Trinh <141846337+Tuyet-Trinh@users.noreply.github.com>
2024-05-27 04:43:52 +0000

    Revert "Merge branch 'refactor' into TrinhT/test"
    This reverts commit 802c4d12772c23a6c4a347689c8604992959418a, reversing
    changes made to 9f77c5fc5c424461a541db7ad8b3904fd79855fa.

--- environment/dockerfiles/entrypoints/odoo.entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-tmp_folder="/usr/lib/python3/dist-packages/odoo/addons.tmp"
-target_folder="/usr/lib/python3/dist-packages/odoo/addons"
-
-if [ -z "$(ls -A $target_folder)" ]; then
-    cp -r $tmp_folder/* $target_folder
-fi
-
-exec "$@"
